c86b12121cd2939f1dc2e14da66fc58da4d8c1d9
matej <matej.basta@email.cz>
2024-04-23 11:39:39 +0200

    header styled

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,14 @@
 import styles from "./header.module.css";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1>Header</h1>
+      <div className={styles.cart}>
+        <FaShoppingCart className={styles["cart-icon"]}/>
+        <div>3</div>
+      </div>
     </header>
   );
 }
