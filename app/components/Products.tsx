2bc680bb03de2d4421171c624efe2c766f37d60d
matej <matej.basta@email.cz>
2024-05-12 21:32:45 +0200

    adding revalidation

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -3,7 +3,7 @@ import Product from './Product';
 import styles from './products.module.css';
 
 async function getData() {
-  const response = await fetch('https://server-for-products.vercel.app/api/products');
+  const response = await fetch('https://server-for-products.vercel.app/api/products', { next: { revalidate: 10000 } });
   return response.json();
 }
 
